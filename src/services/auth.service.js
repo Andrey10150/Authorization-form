f4c3d4e8e0dcf7af0e4c3c66d42711da1c48cea8
Andrey <andrey10150@pop-os.localdomain>
2020-12-08 12:02:57 +0200

    updated url's

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000/api/auth/'
+const API_URL = 'https://authorization-form.herokuapp.com/api/auth/'
 
 class AuthService {
   login(username, password) {
--- src/services/country.service.js
+++ src/services/country.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/countries';
+const API_URL = 'https://authorization-form.herokuapp.com/api/countries';
 
 class CountryService {
   getAllCountries() {
